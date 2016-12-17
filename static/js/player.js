6be08e75b4df81346bbc6cb53f6457dab106db7f
Vitaly Dudarev <vitalydudarev@gmail.com>
2016-12-18 00:08:17 +0300

    Changed results representation.

--- static/js/player.js
+++ static/js/player.js
@@ -3,25 +3,25 @@
         // Setup the player to autoplay the next track
         var a = audiojs.createAll({
           trackEnded: function() {
-            var next = $('ol li.playing').next();
-            if (!next.length) next = $('ol li').first();
+            var next = $('#search_results a.playing').next()
+            if (!next.length) next = $('#search_results a').first();
             next.addClass('playing').siblings().removeClass('playing');
-            audio.load($('a', next).attr('data-src'));
+            audio.load(next.attr('data-src'));
             audio.play();
           }
         });
         
         // Load in the first track
         var audio = a[0];
-            first = $('ol a').attr('data-src');
-        $('ol li').first().addClass('playing');
+            first = $('#search_results a').attr('data-src');
+        $('#search_results a').first().addClass('playing');
         audio.load(first);
 
         // Load in a track on click
-        $('ol li').click(function(e) {
+        $('#search_results a').click(function(e) {
           e.preventDefault();
           $(this).addClass('playing').siblings().removeClass('playing');
-          audio.load($('a', this).attr('data-src'));
+          audio.load($(this).attr('data-src'));
           audio.play();
         });
         // Keyboard shortcuts
