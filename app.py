3e47aba70551c6e72870e686143c97f587919812
Vitaly Dudarev <vitalydudarev@gmail.com>
2017-01-04 00:39:39 +0300

    Show weather forecast on main page.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import vk_api
 import config
 from flask import Flask, render_template, request
+from yahoo_api import YahooWeatherApi
 
 app = Flask(__name__)
 config = config.load_config('config.json')
 api = vk_api.VkApi(config.access_token, config.proxy)
+weather_api = YahooWeatherApi()
 
 @app.route("/")
 def hello():
@@ -24,6 +26,10 @@ def audios():
         return render_template("audios.html")
     if request.method == 'POST':
         return api.get(config.user_id)
+
+@app.route("/weather")
+def weather():
+    return weather_api.get_forecast(834463)
 
 if __name__ == "__main__":
     #app.run(host='0.0.0.0')
