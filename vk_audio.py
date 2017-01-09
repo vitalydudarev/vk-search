ddd711495c09da22b3cf4672d42ab601fe2198b9
Vitaly Dudarev <vitalydudarev@gmail.com>
2017-01-09 23:52:32 +0300

    Return format improvements.

--- vk_audio.py
+++ vk_audio.py
@@ -2,25 +2,49 @@ import requests
 import json
 import urllib
 import re
+from HTMLParser import HTMLParser
 
 class VkAudio:
+    # the constructor takes cookie parameter in the format 'remixsid={id}'
     def __init__(self, cookie):
         self.__url = 'https://vk.com/al_audio.php'
         self.__cookie = cookie
+        self.__parser = HTMLParser()
 
+    # returns an array of Audio objects
     def get_audio_info(self, audio_ids):
         ids = ','.join(str(x) for x in audio_ids)
         headers = {"cookie": self.__cookie}
         params = {"act": "reload_audio", "al": 1, "ids": ids}
 
-        return self.__get_response(headers, params)
+        audio_info = self.__get_response(headers, params)
+        result = []
+
+        for item in audio_info:
+            track_id = item[1] + "_" + item[0]
+            link = item[2]
+            result.append({"track_id": track_id, "link": link})
+
+        if len(result) == 1:
+            return json.dumps(result[0])
+        else:
+            return json.dumps(audio_info)
 
     def get_audio_list(self, owner_id):
         headers = {"cookie": self.__cookie}
         params = {"act": "load_silent", "al": 1, "album_id": -2, "band": False, "owner_id": owner_id}
 
         j_object = self.__get_response(headers, params)
-        return j_object['list']
+        audio_list = j_object['list']
+
+        result = []
+
+        for item in audio_list:
+            track_id = item[1] + "_" + item[0]
+            title = self.__parser.unescape(item[4] + " - " + item[3])
+            result.append({"track_id": track_id, "title": title})
+
+        return json.dumps(result)
 
     def __get_response(self, headers, params):
         encoded_params = urllib.urlencode(params)
