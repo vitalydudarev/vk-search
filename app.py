54e4a81e716fefcfd724603b232567c2db31b58c
Vitaly Dudarev <vitalydudarev@gmail.com>
2017-02-17 01:14:38 +0300

    Moved some logic to Facade class.

--- app.py
+++ app.py
@@ -1,29 +1,17 @@
-import config
-import datetime
-import utils
-import re
-import json
-from vk_api import VkApi
 from flask import Flask, render_template, request
-from storage import Storage
-from vk_audio import VkAudio
-from facade import ServicesFacade
-from client import HttpClient
-        
+import config
+from facade import Facade
 
 app = Flask(__name__)
 config = config.load_config('config.json')
-client = HttpClient(config.proxy, 10)
-vk_api = VkApi(config.access_token, client)
-vk_audio = VkAudio(config.vk_cookie, client)
-storage = Storage()
-services_facade = ServicesFacade(storage, client)
+facade = Facade(config)
+
 
 @app.route("/")
 def home():
-    cur_rate = storage.get('cur_rate')
-    j = json.loads(cur_rate)
-    return render_template("home.html", cur_rate=j['result'].values()[0])
+    rate = facade.get_current_rate()
+    return render_template("home.html", cur_rate=rate)
+
 
 @app.route("/search", methods=['POST', 'GET'])
 def search():
@@ -31,61 +19,39 @@ def search():
         return render_template("search.html")
     if request.method == 'POST':
         query = request.form['value']
-        return vk_audio.search(query)
+        return facade.vk_search(query)
+
 
 @app.route("/audios", methods=['POST', 'GET'])
 def audios():
     if request.method == 'GET':
         return render_template("audios.html")
     if request.method == 'POST':
-        return vk_audio.get_audio_list(config.user_id)
+        return facade.vk_get_audio_list(config.user_id)
+
 
 @app.route("/rates")
 def rates():
     return render_template("rates.html")
 
+
 @app.route("/get_rates")
 def get_rates():
     currency = request.args.get('currency')
     tenor = request.args.get('tenor')
-    if tenor is not None:
-        match = re.search('(\\d+)([mMwW])', tenor)
-        if match is not None:
-            tenor_i = int(match.group(1))
-            m_w = match.group(2)
-            key = 'rates-' + str(tenor_i) + m_w
-            res = storage.get(key)
-            if res is None:
-                end = datetime.date.today()
-                start = None
-                if m_w in ['m', 'M']:
-                    start = utils.month_delta(end, tenor_i * -1)
-                elif m_w in ['w', 'W']:
-                    start = end - datetime.timedelta(days=7 * tenor_i)
-                rates = services_facade.rates().get_rates(currency, start, end)
-                storage.add(key, rates)
-                return rates
-            else:
-                return res
-    else:
-        s_start = request.args.get('start')
-        s_end = request.args.get('end')
-        start = utils.string_to_date(s_start, "%Y-%m-%d")
-        end = utils.string_to_date(s_end, "%Y-%m-%d")
-        return services_facade.rates().get_rates(currency, start, end)
+    return facade.get_rates(currency, tenor)
+
 
 @app.route("/audio_info/<id>")
 def audio_info(id):
-    return vk_audio.get_audio_info([id])
+    return facade.vk_get_audio_info([id])
+
 
 @app.route("/weather")
 def weather():
-    weather = storage.get('weather')
-    if weather is None:
-        weather = services_facade.weather().get_forecast(834463)
-        storage.add('weather', weather)
-    return weather
+    return facade.get_current_weather()
+
 
 if __name__ == "__main__":
-    #app.run(host='0.0.0.0')
+    # app.run(host='0.0.0.0')
     app.run(debug=True)
--- facade.py
+++ facade.py
@@ -1,6 +1,11 @@
+import re
+from client import HttpClient
+from vk_audio import VkAudio
 from wrappers import YahooWeatherWrapper, NbrbRatesWrapper
 from storage import Storage
 import datetime
+import json
+import utils
 
 
 class ServicesFacade:
@@ -20,3 +25,54 @@ class ServicesFacade:
 
     def rates(self):
         return self.__rates_wrapper
+
+
+class Facade:
+    def __init__(self, config):
+        self.__storage = Storage()
+        self.__client = HttpClient(config.proxy, 10)
+        self.__services_facade = ServicesFacade(self.__storage, self.__client)
+        self.__vk_audio = VkAudio(config.vk_cookie, self.__client)
+
+    def get_current_rate(self):
+        cur_rate = self.__storage.get('cur_rate')
+        res = json.loads(cur_rate)
+        return res['result'].values()[0]
+
+    def get_current_weather(self):
+        weather = self.__storage.get('weather')
+        return weather
+
+    def get_rates(self, currency, tenor, start=None, end=None):
+        if tenor is not None:
+            match = re.search('(\\d+)([mMwW])', tenor)
+            if match is not None:
+                tenor_i = int(match.group(1))
+                m_w = match.group(2)
+                key = 'rates-' + str(tenor_i) + m_w
+                res = self.__storage.get(key)
+                if res is None:
+                    end = datetime.date.today()
+                    start = None
+                    if m_w in ['m', 'M']:
+                        start = utils.month_delta(end, tenor_i * -1)
+                    elif m_w in ['w', 'W']:
+                        start = end - datetime.timedelta(days=7 * tenor_i)
+                    rates = self.__services_facade.rates().get_rates(currency, start, end)
+                    self.__storage.add(key, rates)
+                    return rates
+                else:
+                    return res
+        else:
+            s_start = utils.string_to_date(start, "%Y-%m-%d")
+            s_end = utils.string_to_date(end, "%Y-%m-%d")
+            return self.__services_facade.rates().get_rates(currency, s_start, s_end)
+
+    def vk_search(self, query):
+        return self.__vk_audio.search(query)
+
+    def vk_get_audio_list(self, user_id):
+        return self.__vk_audio.get_audio_list(user_id)
+
+    def vk_get_audio_info(self, id):
+        return self.__vk_audio.get_audio_info(id)
