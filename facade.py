8e9acd316bae8e89f84cb6a1068b660963965140
Vitaly Dudarev <vitalydudarev@gmail.com>
2017-03-09 00:04:29 +0300

    Added scheduler to update weather/rates.

--- facade.py
+++ facade.py
@@ -3,36 +3,30 @@ from client import HttpClient
 from vk_audio import VkAudio
 from wrappers import YahooWeatherWrapper, NbrbRatesWrapper
 from storage import Storage
+from scheduler import Scheduler
 import datetime
 import json
 import utils
+import threading
+import logging
 
 
-class ServicesFacade:
-    def __init__(self, storage, client):
-        print 'Initializing services'
-        self.__storage = storage
-        self.__rates_wrapper = NbrbRatesWrapper(client)
-        self.__weather_wrapper = YahooWeatherWrapper(client)
-        weather = self.__weather_wrapper.get_forecast(834463)
-        cur_rate = self.__rates_wrapper.get_rate('USD', datetime.date.today())
-        storage.add('weather', weather)
-        storage.add('cur_rate', cur_rate)
-        print 'Initialized services'
-
-    def weather(self):
-        return self.__weather_wrapper
-
-    def rates(self):
-        return self.__rates_wrapper
+logging.basicConfig(level = logging.DEBUG)
 
 
 class Facade:
     def __init__(self, config):
         self.__storage = Storage()
         self.__client = HttpClient(config.proxy, 10)
-        self.__services_facade = ServicesFacade(self.__storage, self.__client)
         self.__vk_audio = VkAudio(config.vk_cookie, self.__client)
+        self.__rates_service = NbrbRatesWrapper(self.__client)
+        self.__weather_service = YahooWeatherWrapper(self.__client)
+        self.__update_rates()
+        self.__update_weather()
+        self.__rates_scheduler = Scheduler(self.__update_rates)
+        self.__weather_scheduler = Scheduler(self.__update_weather)
+        self.__rates_scheduler.start(3600 * 24, 00, 00)
+        self.__weather_scheduler.start(3600, 00)
 
     def get_current_rate(self):
         cur_rate = self.__storage.get('cur_rate')
@@ -58,7 +52,7 @@ class Facade:
                         start = utils.month_delta(end, tenor_i * -1)
                     elif m_w in ['w', 'W']:
                         start = end - datetime.timedelta(days=7 * tenor_i)
-                    rates = self.__services_facade.rates().get_rates(currency, start, end)
+                    rates = self.__rates_service.get_rates(currency, start, end)
                     self.__storage.add(key, rates)
                     return rates
                 else:
@@ -66,7 +60,7 @@ class Facade:
         else:
             s_start = utils.string_to_date(start, "%Y-%m-%d")
             s_end = utils.string_to_date(end, "%Y-%m-%d")
-            return self.__services_facade.rates().get_rates(currency, s_start, s_end)
+            return self.__rates_service.get_rates(currency, s_start, s_end)
 
     def vk_search(self, query):
         return self.__vk_audio.search(query)
@@ -76,3 +70,13 @@ class Facade:
 
     def vk_get_audio_info(self, id):
         return self.__vk_audio.get_audio_info(id)
+
+    def __update_rates(self):
+        cur_rate = self.__rates_service.get_rate('USD', datetime.date.today())
+        logging.info(u'Updated rates')
+        self.__storage.add('cur_rate', cur_rate)
+
+    def __update_weather(self):
+        weather = self.__weather_service.get_forecast(834463)
+        logging.info(u'Updated weather')
+        self.__storage.add('weather', weather)
---
+++ scheduler.py
@@ -0,0 +1,62 @@
+from threading import Timer
+import time
+import threading
+from datetime import datetime, timedelta
+
+
+class Scheduler():
+    def __init__(self, target):
+        self.is_running = False
+        self.__should_continue = False
+        self.__target = target
+        self.__thread = None
+        self.__time_roller = DateTimeRoller()
+
+    def start(self, interval, minutes, hours=None):
+        self.__interval = interval
+        now = datetime.now()
+        then = self.__time_roller.get_time(now, minutes, hours)
+        sleep_time = (then - now).total_seconds()
+
+        if not self.__should_continue and not self.is_running:
+            self.__should_continue = True
+            self.__start_timer(sleep_time)
+        else:
+            print("Timer already started or running, please wait if you're restarting.")
+
+    def cancel(self):
+        if self.__thread is not None:
+            self.__should_continue = False # Just in case thread is running and cancel fails.
+            self.__thread.cancel()
+        else:
+            print("Timer never started or failed to initialize.")
+
+    def __run(self):
+        self.is_running = True
+        self.__target()
+        self.is_running = False
+        self.__start_timer(self.__interval)
+
+    def __start_timer(self, interval):
+        if self.__should_continue: # Code could have been running when cancel was called.
+            self.__thread = Timer(interval, self.__run)
+            self.__thread.start()
+
+
+class DateTimeRoller:
+    def get_time(self, now, minutes, hours=None):
+        s_hour = now.hour
+        s_minute = now.minute
+
+        if hours == None:
+            if minutes > s_minute:
+                min_delta = minutes - s_minute
+                return now + timedelta(minutes=min_delta)
+            else:
+                return (now + timedelta(hours=1)).replace(minute=minutes)
+        else:
+            if hours > s_hour:
+                hour_delta = hours - s_hour
+                return (now + timedelta(hours=hour_delta)).replace(minute=minutes)
+            else:
+                return (now + timedelta(days=1)).replace(minute=minutes, hour=hours)
