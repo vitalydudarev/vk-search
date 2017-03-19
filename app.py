808d7d5ffc2915d9895da12197197a6af382119a
Vitaly Dudarev <vitalydudarev@gmail.com>
2017-03-19 23:37:28 +0300

    Show currency rates/weather on all pages.

--- app.py
+++ app.py
@@ -9,14 +9,15 @@ facade = Facade(config)
 
 @app.route("/")
 def home():
-    rate = facade.get_current_rate()
-    return render_template("home.html", cur_rate=rate)
+    data = __get_template_data()
+    return render_template("home.html", currency_rate = data['currency_rate'], forecast = data['forecast'])
 
 
 @app.route("/search", methods=['POST', 'GET'])
 def search():
     if request.method == 'GET':
-        return render_template("search.html")
+        data = __get_template_data()
+        return render_template("search.html", currency_rate = data['currency_rate'], forecast = data['forecast'])
     if request.method == 'POST':
         query = request.form['value']
         return facade.vk_search(query)
@@ -25,14 +26,16 @@ def search():
 @app.route("/audios", methods=['POST', 'GET'])
 def audios():
     if request.method == 'GET':
-        return render_template("audios.html")
+        data = __get_template_data()
+        return render_template("audios.html", currency_rate = data['currency_rate'], forecast = data['forecast'])
     if request.method == 'POST':
         return facade.vk_get_audio_list(config.user_id)
 
 
 @app.route("/rates")
 def rates():
-    return render_template("rates.html")
+    data = __get_template_data()
+    return render_template("rates.html", currency_rate = data['currency_rate'], forecast = data['forecast'])
 
 
 @app.route("/get_rates")
@@ -51,6 +54,11 @@ def audio_info(id):
 def weather():
     return facade.get_current_weather()
 
+
+def __get_template_data():
+    rate = facade.get_current_rate()
+    forecast = facade.get_current_weather()
+    return { 'currency_rate': rate, 'forecast': forecast }
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0')
