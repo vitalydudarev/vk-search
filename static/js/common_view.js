eac7248e178adde08f90b65988b70a65ca74a750
Vitaly Dudarev <vitalydudarev@gmail.com>
2017-03-27 20:36:48 +0300

    Handle the case when weather is not available.

--- static/js/common_view.js
+++ static/js/common_view.js
@@ -1,30 +1,34 @@
 function setView(jData) {
   var pData = JSON.parse(jData);
+  if (pData.has_error) {
+    $("#weather a").text("Weather unavailable");
+  }
+  else {
+    $("#weather").attr('data', JSON.stringify(pData))
+    $("#weather a").text(pData.result.condition.temp + '° ' + pData.result.condition.text);
 
-  $("#weather").attr('data', JSON.stringify(pData))
-  $("#weather a" ).text(pData.result.condition.temp + '° ' + pData.result.condition.text);
+    $("#weather").popover({ html: true, animation: true, trigger: "click", placement: 'auto' })
+      .on("show.bs.popover", function () {
+        var _this = this;
+        var data = $('#weather').attr('data');
 
-  $("#weather").popover({ html: true, animation: true, trigger: "click", placement: 'auto' })
-    .on("show.bs.popover", function () {
-      var _this = this;
-      var data = $('#weather').attr('data');
+        var body = "";
+        var jsoned = JSON.parse(data)
 
-      var body = "";
-      var jsoned = JSON.parse(data)
+        var tbody = $('<tbody/>');
+        var table = $('<div/>').attr('class', 'table-responsive')
+          .append($('<table/>').attr('class', 'table table-bordered')
+            .append(tbody));
 
-      var tbody = $('<tbody/>');
-      var table = $('<div/>').attr('class', 'table-responsive')
-        .append($('<table/>').attr('class', 'table table-bordered')
-          .append(tbody));
+        $.each(jsoned.result.forecast, function (index, item) {
+          var td1 = $('<td/>').text(item.day + ", " + item.date);
+          var td2 = $('<td/>').text(item.high + "° " + item.low + "°");
+          var td3 = $('<td/>').text(item.text);
 
-      $.each(jsoned.result.forecast, function (index, item) {
-        var td1 = $('<td/>').text(item.day + ", " + item.date);
-        var td2 = $('<td/>').text(item.high + "° " + item.low + "°");
-        var td3 = $('<td/>').text(item.text);
+          tbody.append($('<tr/>').append(td1, td2, td3));
+        });
 
-        tbody.append($('<tr/>').append(td1, td2, td3));
+        $('#weather').attr('data-content', table.html())
       });
-
-      $('#weather').attr('data-content', table.html())
-    });
+  }
 }
