0eca0f0c1eefc4ee561f453a0c3fff6ad3f01253
Vitaly Dudarev <vitalydudarev@gmail.com>
2017-09-04 21:55:56 +0200

    Check if file exists.

--- rutor.py
+++ rutor.py
@@ -1,12 +1,17 @@
 import json
+import os.path
 
 
 class RutorDataHolder:
+
+    __data = {}
+
     def __init__(self, file_name):
-        with open(file_name, 'r') as file:
-            data = json.loads(file.read())
-            res = [dict(t) for t in set([tuple(d.items()) for d in data])]
-            self.__data = sorted(res, key=lambda k: k['torrent_id'], reverse=True)
+        if os.path.isfile(file_name):
+            with open(file_name, 'r') as file:
+                data = json.loads(file.read())
+                res = [dict(t) for t in set([tuple(d.items()) for d in data])]
+                self.__data = sorted(res, key=lambda k: k['torrent_id'], reverse=True)
 
     def get_data(self, index=0):
         result = [self.__data[i] for i in range(index, index + 100)]
