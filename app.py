6bf87fb39d442af7cf1dde0d5ec1764c99482d08
Vitaly Dudarev <vitalydudarev@gmail.com>
2018-01-22 23:14:21 +0300

    Refactored Facade.

--- app.py
+++ app.py
@@ -1,42 +1,46 @@
 from flask import Flask, render_template, request, make_response
-import config
-from facade import Facade
 from jinja2 import Template
+from facade import Facade, VkAudioFacade, WeatherFacade, RatesFacade
+import config
+
 
 app = Flask(__name__)
 config = config.load_config('config.json')
 facade = Facade(config)
+vk_audio_facade = VkAudioFacade(config)
+weather_facade = WeatherFacade(config)
+rates_facade = RatesFacade(config)
 
 
 @app.route("/")
 def home():
     data = __get_template_data()
-    return render_template("home.html", currency_rate = data['currency_rate'], forecast = data['forecast'], date_formatted=facade.get_date_formatted())
+    return render_template("home.html", currency_rate=data['currency_rate'], forecast=data['forecast'], date_formatted=facade.get_date_formatted())
 
 
 @app.route("/search", methods=['POST', 'GET'])
 def search():
     if request.method == 'GET':
         data = __get_template_data()
-        resp = make_response(render_template("search.html", currency_rate = data['currency_rate'], forecast = data['forecast']))
+        resp = make_response(render_template("search.html", currency_rate=data['currency_rate'], forecast=data['forecast']))
         resp.set_cookie('vkUserId', str(config.user_id))
         return resp
     if request.method == 'POST':
         query = request.form['query']
         offset = request.form['offset']
-        return facade.vk_search(query, offset)
+        return vk_audio_facade.audio_search(query, offset)
 
 
 @app.route("/audios", methods=['POST', 'GET'])
 def audios():
     if request.method == 'GET':
         data = __get_template_data()
-        resp = make_response(render_template("audios.html", currency_rate = data['currency_rate'], forecast = data['forecast']))
+        resp = make_response(render_template("audios.html", currency_rate=data['currency_rate'], forecast=data['forecast']))
         resp.set_cookie('vkUserId', str(config.user_id))
         return resp
     if request.method == 'POST':
         offset = request.form['offset']
-        return facade.vk_get_audio_list(config.user_id, offset)
+        return vk_audio_facade.get_audio_list(config.user_id, offset)
 
 
 @app.route("/rates")
@@ -49,12 +53,12 @@ def rates():
 def get_rates():
     currency = request.args.get('currency')
     tenor = request.args.get('tenor')
-    return facade.get_rates(currency, tenor)
+    return rates_facade.get_rates(currency, tenor)
 
 
 @app.route("/audio_info/<id>")
 def audio_info(id):
-    return facade.vk_get_audio_info([id])
+    return vk_audio_facade.get_audio_info([id])
 
 
 @app.route("/rutor")
@@ -79,22 +83,21 @@ def search_place():
         header = __get_header()
         template = Template(header)
         header_str = template.render(currency_rate=data['currency_rate'])
-        return render_template("search_place.html", header=header_str, forecast = data['forecast'])
+        return render_template("search_place.html", header=header_str, forecast=data['forecast'])
     if request.method == 'POST':
         query = request.form['query']
         return facade.get_place_woeid(query)
 
 
 def __get_template_data():
-    rate = facade.get_current_rate()
-    forecast = facade.get_current_weather()
-    return { 'currency_rate': rate, 'forecast': forecast }
+    rate = rates_facade.get_rate()
+    forecast = weather_facade.get_weather_forecast()
+    return {'currency_rate': rate, 'forecast': forecast}
 
 
 def __get_header():
-        with open('templates/header.html', 'r') as file:
-            print 'opened'
-            return file.read()
+    with open('templates/header.html', 'r') as f:
+        return f.read()
 
 
 if __name__ == "__main__":
--- facade.py
+++ facade.py
@@ -7,9 +7,9 @@ from client import HttpClient
 from scheduler import Scheduler
 from storage import Storage
 from vk_audio import VkAudio
-from wrappers import YahooWeatherWrapper, NbrbRatesWrapper
-from yahoo_api import YahooGeoApi
+from yahoo_api import YahooGeoApi, YahooWeatherApi
 from rutor import RutorDataHolder
+from rates import NbrbRates
 
 logging.basicConfig(level = logging.DEBUG, format = "%(asctime)s;%(levelname)s;%(message)s")
 
@@ -18,30 +18,51 @@ class MyEncoder(json.JSONEncoder):
     def default(self, o):
         return o.__dict__
 
-class Facade:
+class VkAudioFacade:
+    def __init__(self, config):
+        self.__client = HttpClient(config.proxy, 10)
+        self.__vk_audio = VkAudio(config.vk_cookie, self.__client)
+
+    def audio_search(self, query, offset=0):
+        return self.__vk_audio.search(query, offset)
+
+    def get_audio_list(self, user_id, offset=0):
+        return self.__vk_audio.get_playlist(user_id, offset)
+
+    def get_audio_info(self, id):
+        return self.__vk_audio.get_audio_info(id)
+
+class WeatherFacade:
     def __init__(self, config):
         self.__storage = Storage()
         self.__client = HttpClient(config.proxy, 10)
-        self.__vk_audio = VkAudio(config.vk_cookie, self.__client)
-        self.__rates_service = NbrbRatesWrapper(self.__client)
-        self.__weather_service = YahooWeatherWrapper(self.__client)
-        self.__geo_service = YahooGeoApi(self.__client)
-        self.__update_rates()
+        self.__weather_service = YahooWeatherApi(self.__client)
         self.__update_weather()
-        self.__rates_scheduler = Scheduler(self.__update_rates)
         self.__weather_scheduler = Scheduler(self.__update_weather)
-        self.__rates_scheduler.start(3600 * 24, 00, 00)
         self.__weather_scheduler.start(3600, 00)
-        self.__rutor_data_holder = RutorDataHolder('dump.txt')
 
-    def get_current_rate(self):
+    def get_weather_forecast(self):
+        weather = self.__storage.get('weather')
+        return weather
+
+    def __update_weather(self):
+        weather = self.__weather_service.get_forecast(834463)
+        logging.info(u'Updated weather')
+        self.__storage.add('weather', weather)
+
+class RatesFacade:
+    def __init__(self, config):
+        self.__storage = Storage()
+        self.__client = HttpClient(config.proxy, 10)
+        self.__rates_service = NbrbRates(self.__client)
+        self.__update_rates()
+        self.__rates_scheduler = Scheduler(self.__update_rates)
+        self.__rates_scheduler.start(3600 * 24, 00, 00)
+
+    def get_rate(self):
         cur_rate = self.__storage.get('cur_rate')
         res = json.loads(cur_rate)
         return res['result']
-
-    def get_current_weather(self):
-        weather = self.__storage.get('weather')
-        return weather
 
     def get_rates(self, currency, tenor, start=None, end=None):
         if tenor is not None:
@@ -68,14 +89,17 @@ class Facade:
             s_end = utils.string_to_date(end, "%Y-%m-%d")
             return self.__rates_service.get_rates_dynamics(currency, s_start, s_end)
 
-    def vk_search(self, query, offset=0):
-        return self.__vk_audio.search(query, offset)
+    def __update_rates(self):
+        cur_rate = self.__rates_service.get_today_rate('USD')
+        logging.info(u'Updated rates')
+        self.__storage.add('cur_rate', cur_rate)
 
-    def vk_get_audio_list(self, user_id, offset=0):
-        return self.__vk_audio.get_playlist(user_id, offset)
-
-    def vk_get_audio_info(self, id):
-        return self.__vk_audio.get_audio_info(id)
+class Facade:
+    def __init__(self, config):
+        self.__storage = Storage()
+        self.__client = HttpClient(config.proxy, 10)
+        self.__geo_service = YahooGeoApi(self.__client)
+        self.__rutor_data_holder = RutorDataHolder('dump.txt')
 
     def get_date_formatted(self):
         return datetime.date.today().strftime("%A, %d %B %Y")
@@ -86,13 +110,3 @@ class Facade:
     def get_rutor_data(self, index=0):
         data = self.__rutor_data_holder.get_data(index)
         return data
-
-    def __update_rates(self):
-        cur_rate = self.__rates_service.get_today_rate('USD')
-        logging.info(u'Updated rates')
-        self.__storage.add('cur_rate', cur_rate)
-
-    def __update_weather(self):
-        weather = self.__weather_service.get_forecast(834463)
-        logging.info(u'Updated weather')
-        self.__storage.add('weather', weather)
